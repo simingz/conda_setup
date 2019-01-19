e98d1a76f526083c0beaf1074e222abdd55a2a02
simingz <michellezhao.student@gmail.com>
2019-01-18 18:20:09 -0600

    rcc backup

--- backup_env.sh
+++ backup_env.sh
@@ -2,9 +2,10 @@
 # code copied from DavidMertz, https://github.com/conda/conda/issues/5165
 
 NOW=$(date "+%Y-%m-%d")
+mkdir $HOME/conda_setup/RCC/envs-$NOW
 ENVS=$(conda env list | grep '^\w' | cut -d' ' -f1)
 for env in $ENVS; do
     source activate $env
-    conda env export > $HOME/conda_setup/envs-$NOW/$env.yml
+    conda env export > $HOME/conda_setup/RCC/envs-$NOW/$env.yml
     echo "Exporting $env"
 done
